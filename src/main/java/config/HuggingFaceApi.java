adde68c4cc0461775c92ffa136a9b7778410d7fb
SowmyaB <sowmyabatthala@gmail.com>
2025-03-26 16:23:33 +0530

    email classfication

--- src/main/java/config/HuggingFaceApi.java
+++ src/main/java/config/HuggingFaceApi.java
@@ -1,5 +1,9 @@
 package config;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class HuggingFaceApi {
 
+    private static final Logger log = LoggerFactory.getLogger(HuggingFaceApi.class);
 }
